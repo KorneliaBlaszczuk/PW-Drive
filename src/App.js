555ce6c5c6ea5db548e7e441a56261d0d8ef792c
KingaLukiewicz <kingal@itbs.pl>
2025-04-19 00:00:32 +0200

    beginning of header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import './App.css';
+import './App.scss';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>Auto servis</h1>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <header className='App-header'>
+            <img className='App-logo' src='/logo_white.png' alt='App logo' />
+        </header>
+    );
+};
+export default Header;
