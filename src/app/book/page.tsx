a98ee28da55e9ce5130641e11a4f02d0f79dd3eb
KorneliaBlaszczuk <kornelia.blaszczuk@onet.pl>
2025-04-25 11:49:39 +0200

    Added Links to Header pages

---
+++ src/app/book/page.tsx
@@ -0,0 +1,7 @@
+export default function Book() {
+    return (
+        <div>
+            <h1>Umów wizytę</h1>
+        </div>
+    )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,16 @@ import Link from 'next/link';
 import styles from '../styles/Header.module.scss';
 
 export default function Header() {
-    // const navigate = useNavigate();
     return (
         <header className={styles.appHeader}>
-            {/* <Link to="/" className="logoLink"> */}
-            <img className={styles.appLogo} src='/logo_white.png' alt='App logo' />
-            {/* </Link> */}
+            <Link href="/" className="logoLink">
+                <img className={styles.appLogo} src='/logo_white.png' alt='App logo' />
+            </Link>
             <div className={styles.headerCenter}>
                 <Link href="/about" className={styles.aboutUs}>O nas</Link>
-                <p>Umów wizytę</p>
-                <p>Kontakt</p>
-                <p>Cennik</p>
+                <Link href="/book">Umów wizytę</Link>
+                <Link href="contact">Kontakt</Link>
+                <Link href="pricing">Cennik</Link>
             </div>
             <div className={styles.headerRight}>
                 <Button className={styles.logIn} variant="outline">Zaloguj</Button>
