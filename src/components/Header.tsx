da7320baf9a983cc501ce0eb45bb3f711eca898d
KorneliaBlaszczuk <kornelia.blaszczuk@onet.pl>
2025-04-25 12:03:15 +0200

    Improved header buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,23 @@
+'use client';
+
 import { Button } from "@/components/ui/button"
 import Link from 'next/link';
 import styles from '../styles/Header.module.scss';
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+    const pathname = usePathname();
+    const isActive = (path: string) => pathname === path;
     return (
         <header className={styles.appHeader}>
             <Link href="/" className="logoLink">
                 <img className={styles.appLogo} src='/logo_white.png' alt='App logo' />
             </Link>
             <div className={styles.headerCenter}>
-                <Link href="/about" className={styles.aboutUs}>O nas</Link>
-                <Link href="/book">Umów wizytę</Link>
-                <Link href="contact">Kontakt</Link>
-                <Link href="pricing">Cennik</Link>
+                <Link href="/about" className={isActive("/about") ? styles.navActive : styles.navInactive}>O nas</Link>
+                <Link href="/book" className={isActive("/book") ? styles.navActive : styles.navInactive}>Umów wizytę</Link>
+                <Link href="contact" className={isActive("/conntact") ? styles.navActive : styles.navInactive}>Kontakt</Link>
+                <Link href="pricing" className={isActive("/pricing") ? styles.navActive : styles.navInactive}>Cennik</Link>
             </div>
             <div className={styles.headerRight}>
                 <Button className={styles.logIn} variant="outline">Zaloguj</Button>
