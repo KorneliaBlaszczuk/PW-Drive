7f50503d5104512b728f7cb1beb2e92ce1586647
KingaLukiewicz <kingal@itbs.pl>
2025-04-26 16:46:09 +0200

    icons source

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <footer className={styles.appFooter}>
             <p>&copy; 2025 Warsztat XXX</p>
+            <p className={styles.appIcons}>Icons by&nbsp;<a href='https://icons8.com/'>icons8</a></p>
         </footer>
     );
 }
