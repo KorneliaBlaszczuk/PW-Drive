cab7814ce3fa0c94d1a5798cc24de410a17c0e60
KingaLukiewicz <kingal@itbs.pl>
2025-04-29 19:27:36 +0200

    fixed errors caused by fixing dependencies

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import * as React from "react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
+
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
@@ -57,10 +59,18 @@ function Calendar({
                 day_hidden: "invisible",
                 ...classNames,
             }}
+            components={{
+                IconLeft: ({ className, ...props }) => (
+                    <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+                ),
+                IconRight: ({ className, ...props }) => (
+                    <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+                ),
+            }}
             {...props}
         />
     )
 }
-
 Calendar.displayName = "Calendar"
+
 export { Calendar }
