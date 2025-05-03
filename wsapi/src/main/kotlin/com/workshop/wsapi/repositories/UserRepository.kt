9a349a8ccdba196d14d8affe3b5c581b7638fc04
Brydzzz <mrufkazkrem@protonmail.com>
2025-05-03 22:42:43 +0200

    added check for user auth in getCars mapping

--- wsapi/src/main/kotlin/com/workshop/wsapi/repositories/UserRepository.kt
+++ wsapi/src/main/kotlin/com/workshop/wsapi/repositories/UserRepository.kt
@@ -1,7 +1,10 @@
 package com.workshop.wsapi.repositories
 
+import com.workshop.wsapi.models.Car
 import com.workshop.wsapi.models.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.NativeQuery
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -15,8 +18,10 @@ interface UserRepository : JpaRepository<User, Long> {
 
     fun existsByEmail(email: String): Boolean
 
-    @NativeQuery(value = "" +
-            "SELECT * FROM CARS WHERE ID_USER = :id")
+    @NativeQuery(
+        value = "" +
+                "SELECT * FROM CARS WHERE ID_USER = :id"
+    )
     fun getUserCars(@Param("id") id: Long): Optional<List<Car>>
 
 
