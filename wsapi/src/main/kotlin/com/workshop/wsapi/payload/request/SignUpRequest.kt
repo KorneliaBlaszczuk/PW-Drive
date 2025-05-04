fadd4498a94f8eb546692a64599673dc7eac518c
Brydzzz <mrufkazkrem@protonmail.com>
2025-05-04 12:30:00 +0200

    added validation messages for better feedback

--- wsapi/src/main/kotlin/com/workshop/wsapi/payload/request/SignUpRequest.kt
+++ wsapi/src/main/kotlin/com/workshop/wsapi/payload/request/SignUpRequest.kt
@@ -6,24 +6,24 @@ import jakarta.validation.constraints.Size
 
 
 data class SignUpRequest(
-    @field:NotBlank
-    @field:Size(min = 3, max = 20)
+    @field:NotBlank(message = "User cannot be blank")
+    @field:Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     val username: String,
 
     @field:NotBlank
-    @field:Size(max = 50)
-    @field:Email
+    @field:Size(max = 50, message = "Email cannot be longer than 50 characters")
+    @field:Email(message = "Must be a valid email address")
     val email: String,
 
 
-    @field:NotBlank
-    @field:Size(min = 8, max = 120)
+    @field:NotBlank(message = "Password cannot be blank")
+    @field:Size(min = 8, max = 120, message = "Password must have at least 8 characters and not more than 120")
     val password: String,
 
-    @field:NotBlank
+    @field:NotBlank(message = "Name cannot be blank")
     val name: String,
 
-    @field:NotBlank
+    @field:NotBlank(message = "Name cannot be blank")
     val surname: String
 
 )
