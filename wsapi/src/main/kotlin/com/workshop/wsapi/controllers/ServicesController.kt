2484fe19a4496d19754d863974d4660fb3d87af7
Elbergg <tom.p.smolen@gmail.com>
2025-05-11 15:03:23 +0200

    adding and deleting services

--- wsapi/src/main/kotlin/com/workshop/wsapi/controllers/ServicesController.kt
+++ wsapi/src/main/kotlin/com/workshop/wsapi/controllers/ServicesController.kt
@@ -1,14 +1,24 @@
 package com.workshop.wsapi.controllers
 
+import com.workshop.wsapi.models.Service
 import com.workshop.wsapi.models.Visit
+import com.workshop.wsapi.security.isAdmin
+import com.workshop.wsapi.services.ServiceService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
-@RequestMapping("/api/services/")
+@RequestMapping("/api/services")
 class ServicesController {
 
+    @Autowired
+    private lateinit var serviceService: ServiceService
 
     @GetMapping("/{id}")
     fun getServiceById(@PathVariable id: Int): String {
@@ -20,8 +30,22 @@ class ServicesController {
         return "Edited service for user $id"
     }
 
+    @PostMapping("")
+    fun addService(
+        @RequestBody @Validated service: Service,
+        @AuthenticationPrincipal userDetails: UserDetails
+    ): ResponseEntity<Any> {
+        if (!userDetails.isAdmin()) {
+            return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body("You can only access this resource from an Admin Account")
+        }
+        return serviceService.addService(service);
+    }
+
+
     @DeleteMapping("/{id}")
-    fun deleteService(@PathVariable id: Int): String {
-        return "Deleted service for user $id"
+    fun deleteService(@PathVariable id: Long): ResponseEntity<Any> {
+        return serviceService.deleteService(id)
     }
 }
--- wsapi/src/main/kotlin/com/workshop/wsapi/services/ServiceService.kt
+++ wsapi/src/main/kotlin/com/workshop/wsapi/services/ServiceService.kt
@@ -1,4 +1,27 @@
 package com.workshop.wsapi.services
 
+import com.workshop.wsapi.repositories.ServiceRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import com.workshop.wsapi.models.Service as ServiceModel
+
+
+@Service
 class ServiceService {
+
+    @Autowired
+    lateinit var serviceRepository: ServiceRepository
+
+
+    fun addService(service: ServiceModel): ResponseEntity<Any> {
+        
+        val savedService = serviceRepository.save(service)
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedService)
+    }
+
+    fun deleteService(id: Long): ResponseEntity<Any> {
+        return ResponseEntity.status(HttpStatus.OK).body(serviceRepository.deleteById(id))
+    }
 }
