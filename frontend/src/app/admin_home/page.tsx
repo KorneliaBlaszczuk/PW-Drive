4609b3dd583cd6a18d8a7d33627fecf8df489161
KingaLukiewicz <kingal@itbs.pl>
2025-05-11 22:01:12 +0200

    added home page for admin

---
+++ frontend/src/app/admin_home/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function AdminHome() {
+    return (
+        <div className={styles.adminContainer}>
+            <p>To jest strona admina</p>
+        </div>
+    )
+}
