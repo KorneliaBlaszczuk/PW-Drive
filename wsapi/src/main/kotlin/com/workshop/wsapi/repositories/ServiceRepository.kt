337fa046ca15430af4af9a379792db79b5245389
Elbergg <tom.p.smolen@gmail.com>
2025-05-16 18:12:05 +0200

    added getting visits from user

--- wsapi/src/main/kotlin/com/workshop/wsapi/repositories/ServiceRepository.kt
+++ wsapi/src/main/kotlin/com/workshop/wsapi/repositories/ServiceRepository.kt
@@ -1,12 +1,15 @@
 package com.workshop.wsapi.repositories
 
+import com.workshop.wsapi.models.HistoryOfChange
 import com.workshop.wsapi.models.Service
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.NativeQuery
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.util.*
 
 
 @Repository
 interface ServiceRepository : JpaRepository<Service, Long> {
-
-
+    
 }
